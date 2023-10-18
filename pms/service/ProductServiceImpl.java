4a3b08223c3d668df91d7516be7ace1d999b0d4d
Lakshitha Madhushan <42790197+jvlmadhushan@users.noreply.github.com>
2023-10-18 15:16:59 +0530

    Product management backend
    Product management backend with packages and classes

---
+++ pms/service/ProductServiceImpl.java
@@ -0,0 +1,56 @@
+package com.pms.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.pms.model.Product;
+import com.pms.repository.ProductRepository;
+
+public class ProductServiceImpl implements ProductService {
+
+	@Autowired
+	private ProductRepository productRepo;
+
+	@Override
+	public Product saveProduct(Product product) {
+
+		return productRepo.save(product);
+	}
+
+	@Override
+	public List<Product> getAllProduct() {
+		return productRepo.findAll();
+	}
+
+	@Override
+	public Product getProductById(Integer id) {
+		return productRepo.findById(id).get();
+	}
+
+	@Override
+	public String deleteProduct(Integer id) {
+		Product product = productRepo.findById(id).get();
+
+		if (product != null) {
+			productRepo.delete(product);
+			return "Product Delete Sucessfully";
+		}
+
+		return "Something wrong on server";
+	}
+
+	@Override
+	public Product editProduct(Product p, Integer id) {
+
+		Product oldProduct = productRepo.findById(id).get();
+
+		oldProduct.setProductName(p.getProductName());
+		oldProduct.setDescription(p.getDescription());
+		oldProduct.setPrice(p.getPrice());
+		oldProduct.setStatus(p.getStatus());
+
+		return productRepo.save(oldProduct);
+	}
+
+}
